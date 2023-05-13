d5f623479b9e7495873ceb35aad2ea8d6566ae49
kiana <kiana.sherafati@gmail.com>
2023-05-13 20:21:42 +0330

    feat : Added NoteBook class

---
+++ src/NoteBook.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+public class NoteBook {
+    Scanner scanner = new Scanner(System.in);
+    ArrayList<Note> notes = new ArrayList<>();
+
+
+}
