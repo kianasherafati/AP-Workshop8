6eb129460ff453798b14c3b9466c88960dba58b7
kiana <kiana.sherafati@gmail.com>
2023-05-13 20:22:52 +0330

    feat : Added readFile method

--- src/NoteBook.java
+++ src/NoteBook.java
@@ -5,5 +5,13 @@ public class NoteBook {
     Scanner scanner = new Scanner(System.in);
     ArrayList<Note> notes = new ArrayList<>();
 
-
+    public ArrayList<Note> readFile() throws IOException, ClassNotFoundException {
+        FileInputStream fIn = new FileInputStream("notebook.bin");
+        ObjectInputStream in = new ObjectInputStream(fIn);
+        ArrayList<Note> temp = new ArrayList<>();
+        temp = (ArrayList<Note>) in.readObject();
+        fIn.close();
+        in.close();
+        return temp;
+    }
 }
