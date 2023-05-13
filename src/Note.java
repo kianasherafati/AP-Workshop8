44b00016e9ba191632ef2956fa771ba8d0a437c5
kiana <kiana.sherafati@gmail.com>
2023-05-14 00:43:26 +0330

    fix : Edited classes and Fixed bugs in classes

--- src/Note.java
+++ src/Note.java
@@ -1,18 +1,34 @@
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Note implements Serializable{
     String topic;
     String body;
+    Random random = new Random();
+    private int day = random.nextInt(30);
+    private int month = random.nextInt(12);
     public Note(String topic, String body) {
         this.topic = topic;
         this.body = body;
     }
 
+
+    public String getTopic(){
+        return topic;
+    }
+
     @Override
     public String toString() {
-        return "Note{" +
-                "topic='" + topic + '\'' +
-                ", body='" + body + '\'' +
-                '}';
+        return  "----    "+topic + "    ----"  +'\n'+ body + "       ";
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public int getMonth() {
+        return month;
     }
 
 }
--- src/NoteBook.java
+++ src/NoteBook.java
@@ -6,12 +6,19 @@ public class NoteBook {
     ArrayList<Note> notes = new ArrayList<>();
 
     public ArrayList<Note> readFile() throws IOException, ClassNotFoundException, FileNotFoundException, EOFException {
-        FileInputStream fIn = new FileInputStream("notebook.bin");
-        ObjectInputStream in = new ObjectInputStream(fIn);
         ArrayList<Note> temp = new ArrayList<>();
-        temp = (ArrayList<Note>) in.readObject();
-        fIn.close();
-        in.close();
+        try(ObjectInputStream in = new ObjectInputStream(new FileInputStream("notebook.bin"))) {
+            temp = (ArrayList<Note>) in.readObject();
+        }
+        catch (FileNotFoundException fileNotFoundException){
+            System.out.println("Could not find the file!");
+        }
+        catch (IOException e){
+            System.out.println("I/O error occurred!");
+        }
+        catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
         return temp;
     }
 
@@ -20,31 +27,41 @@ public class NoteBook {
     }
 
     public void write() throws IOException {
-        FileOutputStream  fOut = new FileOutputStream("notebook.bin");
-        ObjectOutputStream out = new ObjectOutputStream(fOut);
-        ArrayList<Note> temp = notes;
-        out.writeObject(temp);
-        fOut.close();
-        out.close();
+        ArrayList<Note> tmp = new ArrayList<>();
+        try(ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("notebook.bin"))) {
+            tmp = notes;
+            out.writeObject(tmp);
+        }
+        catch (FileNotFoundException fileNotFoundException){
+            System.out.println("Could not find the file!");
+        }
+        catch (IOException e) {
+            System.out.println("I/O error occurred!");
+        }
     }
 
     public void addNote() throws IOException, ClassNotFoundException {
         System.out.println("please choose a topic for the note or enter '0' to go back to the main menu :");
         String topic = scanner.nextLine();
+        notes = readFile();
         if (topic.equals("0")){
-            showMenu();
+            return;
+        }
+        for (Note note : notes){
+            if (topic.equals(note.getTopic())){
+                System.out.println("Duplicate title!");
+                return;
+            }
         }
         System.out.println("ok. feel free to write:)");
         System.out.println("enter 'finish' to finish!");
         String body = scanner.nextLine();
-        if (!body.equals("finish")){
-            body = scanner.nextLine();
-        }
-        else {
-            System.out.println("the new note has been added successfully");
+        if (body.equals("finish")) {
+            return;
         }
         Note note = new Note(topic,body);
         notes.add(note);
+        System.out.println("the new note has been added successfully");
         write();
     }
 
@@ -52,7 +69,7 @@ public class NoteBook {
         notes = readFile();
         int i = 1;
         for (Note note : notes) {
-            System.out.println(i + "- " + note.toString());
+            System.out.println(i + "- " + note.getTopic() +"          "+ "2023-"+note.getMonth()+"-"+note.getDay());
             i++;
         }
     }
@@ -65,7 +82,7 @@ public class NoteBook {
         printList();
         String choice = scanner.nextLine();
         if(choice.equals("0")){
-            showMenu();
+            return;
         }
         notes.remove(Integer.parseInt(choice)-1);
         System.out.println("the note has been removed successfully!");
@@ -79,23 +96,23 @@ public class NoteBook {
         ArrayList<Note> temp = readFile();
 
         if(choice.equals("0")){
-            showMenu();
+            return;
         }
-        temp.get(Integer.parseInt(choice)-1).toString();
+        System.out.println(temp.get(Integer.parseInt(choice)-1).toString());
     }
 
     public void export() throws IOException, ClassNotFoundException {
         System.out.println("choose a note to export or enter '0' to go back to the main menu :");
         printList();
         int choice = scanner.nextInt();
+        if(choice==0){
+            return;
+        }
         ArrayList<Note>temp = readFile();
         FileOutputStream fOut = new FileOutputStream("export.txt");
         ObjectOutputStream out = new ObjectOutputStream(fOut);
         out.writeObject(temp.get(choice - 1));
         System.out.println("the note has been successfully exported!");
-        System.out.println("you can find it on" + "\"" + "export" + "\"" + "directory.");
-        if(choice==0){
-            showMenu();
-        }
+        System.out.println("you can find it on" + " \"" + "export" + "\" " + "directory.");
     }
 }
