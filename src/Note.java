e6ada0d2f83ed26cb1383eb51069188f03127d0d
kiana <kiana.sherafati@gmail.com>
2023-05-13 19:58:27 +0330

    feat : completed Note class

--- src/Note.java
+++ src/Note.java
@@ -2,5 +2,17 @@ import java.io.Serializable;
 public class Note implements Serializable{
     String topic;
     String body;
+    public Note(String topic, String body) {
+        this.topic = topic;
+        this.body = body;
+    }
+
+    @Override
+    public String toString() {
+        return "Note{" +
+                "topic='" + topic + '\'' +
+                ", body='" + body + '\'' +
+                '}';
+    }
 
 }
